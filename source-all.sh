09087b8502abc061966fd03fcba27e836eb4ed9b
AceOfKestrels <aceofkestrels@gmail.com>
2025-05-05 21:54:00 +0200

    Remove recursive source

--- source-all.sh
+++ source-all.sh
@@ -5,14 +5,6 @@ __source_all() {
     for f in "$SHELL_SOURCES_DIR/$1"/*.sh; do
         source "$f"
     done
-
-    # Recursively source subdirectories
-    for d in "$SHELL_SOURCES_DIR/$1"/*; do
-        d=$(basename "$d")
-        if [ -d "$SHELL_SOURCES_DIR/$1/$d" ]; then
-            __source_all "$1/$d"
-        fi
-    done
 }
 
 __source_all "shell"
