4cbf2c5a6f82cb0931addfa00d99dcd93e69a85c
kes@work <aceofkestrels@gmail.com>
2025-07-16 10:05:11 +0200

    Add more docker compose aliases

--- shell/misc.sh
+++ shell/misc.sh
@@ -3,7 +3,11 @@
 alias cls="clear"
 alias q="exit"
 alias ed="editor"
+
 alias dc="docker compose"
+alias dcu="docker compose up"
+alias dcb="docker compose up --build"
+alias dcd="docker compose down"
 
 editor() {
     if [ -z "$1" ]; then
