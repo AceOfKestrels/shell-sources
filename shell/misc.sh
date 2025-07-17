e5bbb645978fbffcb6ce7f20ca2d95b093d83ecb
kes@work <aceofkestrels@gmail.com>
2025-07-17 11:10:57 +0200

    Add aliases for docker compose logs

--- shell/misc.sh
+++ shell/misc.sh
@@ -5,13 +5,18 @@ alias q="exit"
 alias ed="editor"
 
 alias dc="docker compose"
-alias dcu="docker compose up"
-alias dcb="docker compose up --build"
+alias dcu="docker compose up -d"
+alias dcb="docker compose up -d --build"
 alias dcd="docker compose down"
+alias dcf="docker compose logs --follow --tail 10"
 
 dcr() {
     docker compose down "$@"
     docker compose up --build -d "$@"
+}
+
+dcl() {
+    docker compose logs "$@" | less -R +G
 }
 
 editor() {
