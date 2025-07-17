0525268537beb95807a7be76ae89a80545785430
kes@work <aceofkestrels@gmail.com>
2025-07-17 10:49:18 +0200

    Add another docker compose alias

--- shell/misc.sh
+++ shell/misc.sh
@@ -9,6 +9,11 @@ alias dcu="docker compose up"
 alias dcb="docker compose up --build"
 alias dcd="docker compose down"
 
+dcr() {
+    docker compose down "$@"
+    docker compose up --build -d "$@"
+}
+
 editor() {
     if [ -z "$1" ]; then
         code .
