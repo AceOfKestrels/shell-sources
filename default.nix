c5a26e4637bb58716bcf9ea3e30be55f7e477d2d
Kes <67107510+AceOfKestrels@users.noreply.github.com>
2025-09-20 18:02:38 +0200

    add flake (#6)
    * create flake
    
    * fix option
    
    * enable config options
    
    * source file

---
+++ default.nix
@@ -0,0 +1,16 @@
+{ lib, config, ... }:
+
+let
+    cfg = config.programs.shellSources;
+in
+{
+    options = {
+        programs.shellSources.enable = lib.mkEnableOption "enable shell kes shell sources";
+    };
+
+    config = lib.mkIf cfg.enable {
+        environment.interactiveShellInit = ''
+            . ${./source-all.sh}
+        '';
+    };
+}
