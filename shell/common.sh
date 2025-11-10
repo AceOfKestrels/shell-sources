8c416340a60745d10969a3d72f9bab4a7d421d7b
AceOfKestrels <git@kestrels.dev>
2025-11-10 23:03:59 +0100

    Update reload command

--- shell/common.sh
+++ shell/common.sh
@@ -1,29 +1,34 @@
 #! /bin/bash
 
-if [ -n "$BASH_VERSION" ]; then
+if command -v bind >/dev/null 2>&1; then
     bind "set completion-ignore-case on"
     bind "set show-all-if-ambiguous on"
 fi
 
 reload() {
-    echo "Reloading..."
-    if [ -z "$1" ]
-    then
+    if [ -z "$1" ]; then
+        echo "Reloading shell..."
         exec $SHELL
     fi
 
-    if [ "$1" = "-u" ] || [ "$1" = "--update" ]
-    then
+    if [ "$1" = "-u" ] || [ "$1" = "--update" ]; then
         cd "$SHELL_SOURCES_DIR" >/dev/null || return 1
         git pull 
         cd - >/dev/null 2>&1 || return 1
+        echo
+        echo "Reloading shell..."
         exec $SHELL
     fi
 
     echo -e "${F_FG_BLUE}usage${F_RESET}: reload [options]"
-    echo
-    echo "reload shell profile"
-    echo
-    echo -e "${F_FG_BLUE}options:${F_RESET}"
-    echo -e "    ${F_FG_YELLOW}-u --update  ${F_RESET}Pull new changes from remote before reloading"
+
+    if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+        echo
+        echo "reload shell profile"
+        echo
+        echo -e "${F_FG_BLUE}options:${F_RESET}"
+        echo -e "    ${F_FG_YELLOW}-u --update  ${F_RESET}Pull new changes from remote before reloading"
+    fi
+
+    return 1
 }
