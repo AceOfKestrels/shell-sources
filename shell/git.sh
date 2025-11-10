2c3877c5e9b412c6569cd706addd637db9721e1e
AceOfKestrels <git@kestrels.dev>
2025-11-10 22:59:16 +0100

    Fix check for git completion

--- shell/git.sh
+++ shell/git.sh
@@ -8,7 +8,7 @@ alias gpl='git pull'
 alias pfusch='gp -f'
 alias gpr="git-prune"
 
-if [ -n "$BASH_VERSION" ]; then
+if command -v __git_complete; then
     __git_complete gp _git_checkout
     __git_complete gco _git_checkout
     __git_complete gr _git_checkout
